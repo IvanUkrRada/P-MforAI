3e55336943c56ac26fe1b8cd39daf5d1c415b5a4
SujitBhatta21 <bhattasujit9@gmail.com>
2025-11-26 09:04:52 +0000

    Basic Structure for Task-1
    Testing whether collaboration is properly set up on our group repository.

---
+++ task_1/Activation_Function.py
@@ -0,0 +1,12 @@
+# Class that stores stores all the activation functions.
+
+class Activation_Function:
+    def __init__(self):
+        pass
+
+    def sigmoid(self, ):
+        pass
+
+    def RELU (self, ):
+        pass
+
---
+++ task_1/Main.py
@@ -0,0 +1,11 @@
+### Import necessary libraries.
+
+import Activation_Function as activation_function
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
