7ca61a412c0304de459e22fc03a3d1fcf1b12945
CartesianM <ionixiftier@gmail.com>
2025-12-18 17:36:40 +0000

    added second optimiser SGD+ momentum

--- task_1/Optimisers.py
+++ task_1/Optimisers.py
@@ -21,3 +21,26 @@ class SGD(Optimizer):
 
 
 
+class SGDMomentum(Optimizer):
+    def __init__(self, lr=0.01, beta=0.9):
+        self.lr = lr
+        self.beta = beta
+        self.v = {}
+
+    def update(self, params: dict, grads: dict):
+        for k in params:
+            if k.startswith("W"):
+                i = k[1:]
+                g = grads[f"dW{i}"]
+            elif k.startswith("b"):
+                i = k[1:]
+                g = grads[f"db{i}"]
+            else:
+                continue
+
+            if k not in self.v:
+                self.v[k] = np.zeros_like(params[k])
+
+            # momentum update
+            self.v[k] = self.beta * self.v[k] + g
+            params[k] -= self.lr * self.v[k]
