8c7dec6b0e0f26f40fae2e1630d97df9e6b83790
CartesianM <ionixiftier@gmail.com>
2025-12-18 17:33:22 +0000

    finished the SGD optimiser

--- task_1/Optimisers.py
+++ task_1/Optimisers.py
@@ -3,6 +3,21 @@ Code for optimiser that I can import in neural network.
 """
 import numpy as np
 
-class Optmiser:
+class Optimizer:
     def update(self, params: dict, grads: dict):
-        pass
+        raise NotImplementedError
+
+class SGD(Optimizer):
+    def __init__(self, lr=0.01):
+        self.lr = lr
+    def update(self, params: dict, grads: dict):
+        for k in params:
+            if k.startswith("W"):
+                i = k[1:]
+                params[k] -= self.lr * grads[f"dW{i}"]
+            elif k.startswith("b"):
+                i = k[1:]
+                params[k] -= self.lr * grads[f"db{i}"]
+
+
+
